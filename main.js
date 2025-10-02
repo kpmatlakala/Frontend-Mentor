b1d1e351339b794f7c6e4666d6adef78a0c03bc0
Kabelo Matlakala <matlakalakabelo1@gmail.com>
2025-10-02 14:43:08 +0200

    update readmefor qr and social

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ import { initThemeToggle } from './scripts/theme-toggle.js';
 import { initAccordion } from './scripts/accordion.js';
 import { renderProjects } from './scripts/projects-renderer.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', 
+  () => {
   initThemeToggle();
   initAccordion();
   renderProjects();
