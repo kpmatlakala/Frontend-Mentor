d55cf72a7c4b303e850bff2aa747db8cc3b2d844
Kabelo P. Matlakala <matlakalakabelo1@gmail.com>
2025-10-17 12:09:25 +0300

    get 3-column-preview ready

--- scripts/projects-data.js
+++ scripts/projects-data.js
@@ -1,77 +1,95 @@
 const projectsData = {
-  'Getting Started': [
+  "Getting Started": [
     {
-      title: 'QR Code Component',
-      imageUrl: '/assets/previews/getting-started/qr-preview.jpg',
-      description: 'A clean QR code component focusing on basic HTML structure and CSS styling techniques.',
-      technologies: ['HTML', 'CSS'],
+      title: "QR Code Component",
+      imageUrl: "/assets/previews/getting-started/qr-preview.jpg",
+      description:
+        "A clean QR code component focusing on basic HTML structure and CSS styling techniques.",
+      technologies: ["HTML", "CSS"],
       level: 1,
-      demoUrl: '/Getting_Started/qr-code-component/index.html',
-      feSolutionLink: 'https://www.frontendmentor.io/solutions/qr-code-component-using-html-and-css-qz8jEXNqCK',
-      gitRepoLink: 'https://github.com/kpmatlakala/Frontend-Mentor/tree/qr-code-component',
+      demoUrl: "/Getting_Started/qr-code-component/index.html",
+      feSolutionLink:
+        "https://www.frontendmentor.io/solutions/qr-code-component-using-html-and-css-qz8jEXNqCK",
+      gitRepoLink:
+        "https://github.com/kpmatlakala/Frontend-Mentor/tree/qr-code-component",
     },
     {
-      title: 'Blog Preview Card',
-      imageUrl: '/assets/previews/getting-started/blog-preview.jpg',
-      description: 'An interactive blog card showcasing post previews with images and author information.',
-      technologies: ['HTML', 'CSS'],
+      title: "Blog Preview Card",
+      imageUrl: "/assets/previews/getting-started/blog-preview.jpg",
+      description:
+        "An interactive blog card showcasing post previews with images and author information.",
+      technologies: ["HTML", "CSS"],
       level: 1,
-      demoUrl: '/Getting_Started/blog-preview-card/index.html',
-      feSolutionLink: 'https://www.frontendmentor.io/solutions/blog-preview-card-using-html-and-css-P1kOOz4yMZ',
-      gitRepoLink: 'https://github.com/kpmatlakala/Frontend-Mentor/tree/blog-preview-card',
+      demoUrl: "/Getting_Started/blog-preview-card/index.html",
+      feSolutionLink:
+        "https://www.frontendmentor.io/solutions/blog-preview-card-using-html-and-css-P1kOOz4yMZ",
+      gitRepoLink:
+        "https://github.com/kpmatlakala/Frontend-Mentor/tree/blog-preview-card",
     },
     {
-      title: 'Social Links Profile',
-      imageUrl: '/assets/previews/getting-started/social-preview.jpg',
-      description: 'A clean profile page displaying social media links with hover effects.',
-      technologies: ['HTML', 'CSS'],
+      title: "Social Links Profile",
+      imageUrl: "/assets/previews/getting-started/social-preview.jpg",
+      description:
+        "A clean profile page displaying social media links with hover effects.",
+      technologies: ["HTML", "CSS"],
       level: 1,
-      demoUrl: '/Getting_Started/social-links-profile/index.html',
-      feSolutionLink: 'https://www.frontendmentor.io/solutions/social-links-profile-using-html-and-css-caubdzb98Z',
-      gitRepoLink: 'https://github.com/kpmatlakala/Frontend-Mentor/tree/social-links-profile',
+      demoUrl: "/Getting_Started/social-links-profile/index.html",
+      feSolutionLink:
+        "https://www.frontendmentor.io/solutions/social-links-profile-using-html-and-css-caubdzb98Z",
+      gitRepoLink:
+        "https://github.com/kpmatlakala/Frontend-Mentor/tree/social-links-profile",
     },
     {
-      title: 'Recipe Page',
-      imageUrl: '/assets/previews/getting-started/recipe-preview.jpg',
-      description: 'A structured recipe page with ingredients, instructions, and preparation details.',
-      technologies: ['HTML', 'CSS'],
+      title: "Recipe Page",
+      imageUrl: "/assets/previews/getting-started/recipe-preview.jpg",
+      description:
+        "A structured recipe page with ingredients, instructions, and preparation details.",
+      technologies: ["HTML", "CSS"],
       level: 1,
-      demoUrl: '/Getting_Started/recipe-page/index.html',
-      feSolutionLink: ' https://www.frontendmentor.io/solutions/recipe-page-using-html-and-css-18WbBQzhlc',
-      gitRepoLink: 'https://github.com/kpmatlakala/Frontend-Mentor/tree/recipe-page',
-
-    }
+      demoUrl: "/Getting_Started/recipe-page/index.html",
+      feSolutionLink:
+        " https://www.frontendmentor.io/solutions/recipe-page-using-html-and-css-18WbBQzhlc",
+      gitRepoLink:
+        "https://github.com/kpmatlakala/Frontend-Mentor/tree/recipe-page",
+    },
   ],
-  'Responsive Design Fundamentals': [
+  "Responsive Design Fundamentals": [
     {
-      title: 'Product Preview Card Component',
-      imageUrl: '/assets/previews/responsive-designs/product-card-preview.jpg',
-      description: 'A responsive product card showcasing items with images and pricing.',
-      technologies: ['HTML', 'CSS'],
+      title: "Product Preview Card Component",
+      imageUrl: "/assets/previews/responsive-designs/product-card-preview.jpg",
+      description:
+        "A responsive product card showcasing items with images and pricing.",
+      technologies: ["HTML", "CSS"],
       level: 1,
-      demoUrl: '/Responsive_Designs/product-preview-card-component/',
-      feSolutionLink: 'https://www.frontendmentor.io/solutions/product-preview-card-component-using-html-and-css-1QZ5Ky36qO',
-      gitRepoLink: 'https://github.com/kpmatlakala/Frontend-Mentor/tree/product-preview-card-component',
+      demoUrl: "/Responsive_Designs/product-preview-card-component/",
+      feSolutionLink:
+        "https://www.frontendmentor.io/solutions/product-preview-card-component-using-html-and-css-1QZ5Ky36qO",
+      gitRepoLink:
+        "https://github.com/kpmatlakala/Frontend-Mentor/tree/product-preview-card-component",
     },
     {
-      title: 'Four Card Feature Section',
-      imageUrl: '/assets/previews/responsive-designs/four-cards-preview.jpg',
-      description: 'A responsive grid layout displaying four feature cards.',
-      technologies: ['HTML', 'CSS Grid'],
+      title: "Four Card Feature Section",
+      imageUrl: "/assets/previews/responsive-designs/four-cards-preview.jpg",
+      description: "A responsive grid layout displaying four feature cards.",
+      technologies: ["HTML", "CSS Grid"],
       level: 1,
-      demoUrl: '/Responsive_Designs/four-card-feature-section/',
-      feSolutionLink: 'https://www.frontendmentor.io/solutions/four-card-feature-section-using-css-grid-viZcnudZ-b',
-      gitRepoLink: 'https://github.com/kpmatlakala/Frontend-Mentor/tree/four-card-feature-section',
+      demoUrl: "/Responsive_Designs/four-card-feature-section/",
+      feSolutionLink:
+        "https://www.frontendmentor.io/solutions/four-card-feature-section-using-css-grid-viZcnudZ-b",
+      gitRepoLink:
+        "https://github.com/kpmatlakala/Frontend-Mentor/tree/four-card-feature-section",
     },
     {
-      title: 'Testimonials Grid Section',
-      imageUrl: '/assets/previews/responsive-designs/testimonials-preview.jpg',
-      description: 'A responsive grid showcasing customer testimonials.',
-      technologies: ['HTML', 'CSS Grid'],
+      title: "Testimonials Grid Section",
+      imageUrl: "/assets/previews/responsive-designs/testimonials-preview.jpg",
+      description: "A responsive grid showcasing customer testimonials.",
+      technologies: ["HTML", "CSS Grid"],
       level: 2,
-      demoUrl: '/Responsive_Designs/testimonials-grid-section/',
-      feSolutionLink: 'https://www.frontendmentor.io/solutions/testimonials-grid-section-using-css-grid-b2hBep6Of9',
-      gitRepoLink: 'https://github.com/kpmatlakala/Frontend-Mentor/tree/testimonials-grid-section',
+      demoUrl: "/Responsive_Designs/testimonials-grid-section/",
+      feSolutionLink:
+        "https://www.frontendmentor.io/solutions/testimonials-grid-section-using-css-grid-b2hBep6Of9",
+      gitRepoLink:
+        "https://github.com/kpmatlakala/Frontend-Mentor/tree/testimonials-grid-section",
     },
     // {
     //   title: 'Meet landing page',
@@ -84,80 +102,96 @@ const projectsData = {
     //   comingSoon: true
     // }
   ],
-  'Challenges': [
+  Challenges: [
     {
-      title: 'Weather App',
-      imageUrl: '/assets/previews/challenges/weather-app-preview.jpg',
-      description: 'Full-stack weather application with real-time data and forecasts.',
-      technologies: ['HTML', 'CSS', 'JavaScript', 'API'],
-      tags: ['hackathon', 'responsive', 'real-world', 'api-integration'],
+      title: "Weather App",
+      imageUrl: "/assets/previews/challenges/weather-app-preview.jpg",
+      description:
+        "Full-stack weather application with real-time data and forecasts.",
+      technologies: ["HTML", "CSS", "JavaScript", "API"],
+      tags: ["hackathon", "responsive", "real-world", "api-integration"],
       level: 3,
-      demoUrl: '/Challenges/weather-app/',
+      demoUrl: "/Challenges/weather-app/",
       // gitRepoLink: 'https://github.com/kpmatlakala/Frontend-Mentor/tree/weather-app',
       isGhost: true,
       comingSoon: true,
       isHackathon: true,
-      hackathonName: 'FM30 Hackathon 2025'
+      hackathonName: "FM30 Hackathon 2025",
     },
     {
-      title: 'NFT preview card component',
-      imageUrl: '/assets/previews/challenges/nft-preview.jpg',
-      description: 'Interactive NFT preview card with hover effects and image overlay.',
-      technologies: ['HTML', 'CSS'],
+      title: "NFT preview card component",
+      imageUrl: "/assets/previews/challenges/nft-preview.jpg",
+      description:
+        "Interactive NFT preview card with hover effects and image overlay.",
+      technologies: ["HTML", "CSS"],
       level: 1,
-      demoUrl: '/Challenges/nft-preview-card-component',
+      demoUrl: "/Challenges/nft-preview-card-component",
       //  feSolutionLink: 'https://www.frontendmentor.io/solutions/',
-      gitRepoLink: 'https://github.com/kpmatlakala/Frontend-Mentor/tree/nft-preview-card-component',
+      gitRepoLink:
+        "https://github.com/kpmatlakala/Frontend-Mentor/tree/nft-preview-card-component",
     },
     {
-      title: 'FAQ Accordion',
-      imageUrl: '/assets/previews/challenges/faq-preview.jpg',
-      description: 'Interactive FAQ component with expandable answers and smooth animations.',
-      technologies: ['HTML', 'CSS', 'JavaScript'],
+      title: "FAQ Accordion",
+      imageUrl: "/assets/previews/challenges/faq-preview.jpg",
+      description:
+        "Interactive FAQ component with expandable answers and smooth animations.",
+      technologies: ["HTML", "CSS", "JavaScript"],
       level: 1,
-      demoUrl: '/Challenges/faq-accordion/',
+      demoUrl: "/Challenges/faq-accordion/",
       //  feSolutionLink: 'https://www.frontendmentor.io/solutions/',
-      gitRepoLink: 'https://github.com/kpmatlakala/Frontend-Mentor/tree/faq-accordion',
+      gitRepoLink:
+        "https://github.com/kpmatlakala/Frontend-Mentor/tree/faq-accordion",
     },
     {
-      title: 'Ping Coming Soon Page',
-      imageUrl: '/assets/previews/challenges/ping-preview.jpg',
-      description: 'Dynamic coming soon page with countdown timer and email signup.',
-      technologies: ['HTML', 'CSS', 'JavaScript'],
+      title: "Ping Coming Soon Page",
+      imageUrl: "/assets/previews/challenges/ping-preview.jpg",
+      description:
+        "Dynamic coming soon page with countdown timer and email signup.",
+      technologies: ["HTML", "CSS", "JavaScript"],
       level: 1,
-      demoUrl: '/Challenges/ping-coming-soon-page/',
+      demoUrl: "/Challenges/ping-coming-soon-page/",
       //  feSolutionLink: 'https://www.frontendmentor.io/solutions/',
-      gitRepoLink: 'https://github.com/kpmatlakala/Frontend-Mentor/tree/ping-coming-soon-page'
+      gitRepoLink:
+        "https://github.com/kpmatlakala/Frontend-Mentor/tree/ping-coming-soon-page",
     },
     {
-      title: 'Huddle Landing Page',
-      imageUrl: '/assets/previews/challenges/huddle-preview.jpg',
-      description: 'Modern landing page with hero section and feature highlights.',
-      technologies: ['HTML', 'CSS'],
+      title: "Huddle Landing Page",
+      imageUrl: "/assets/previews/challenges/huddle-preview.jpg",
+      description:
+        "Modern landing page with hero section and feature highlights.",
+      technologies: ["HTML", "CSS"],
       level: 2,
-      demoUrl: '/Challenges/huddle-landing-page-with-single-introductory-section/',
+      demoUrl:
+        "/Challenges/huddle-landing-page-with-single-introductory-section/",
       //  feSolutionLink: 'https://www.frontendmentor.io/solutions/',
-      gitRepoLink: 'https://github.com/kpmatlakala/Frontend-Mentor/tree/huddle-landing-page-with-single-introductory-section'
+      gitRepoLink:
+        "https://github.com/kpmatlakala/Frontend-Mentor/tree/huddle-landing-page-with-single-introductory-section",
     },
     {
-      title: 'Intro Component with Signup Page',
-      imageUrl: '/assets/previews/challenges/signup-intro-preview.jpg',
-      description: 'Responsive signup form with real-time validation and error handling.',
-      technologies: ['HTML', 'CSS', 'JavaScript'],
-      level: 1,      
-      demoUrl: '/Challenges/intro-component-with-signup-form/',
-      //  feSolutionLink: 'https://www.frontendmentor.io/solutions/',
-      gitRepoLink: 'https://github.com/kpmatlakala/Frontend-Mentor/tree/intro-component-with-signup-form'
+      title: "Intro Component with Signup Page",
+      imageUrl: "/assets/previews/challenges/signup-intro-preview.jpg",
+      description:
+        "Responsive signup form with real-time validation and error handling.",
+      technologies: ["HTML", "CSS", "JavaScript"],
+      level: 1,
+      demoUrl: "/Challenges/intro-component-with-signup-form/",
+      feSolutionLink:
+        "https://www.frontendmentor.io/solutions/intro-component-with-signup-page-using-html-css-and-js-PIIQkOde45",
+      gitRepoLink:
+        "https://github.com/kpmatlakala/Frontend-Mentor/tree/intro-component-with-signup-form",
     },
     {
-      title: 'Intro Component with Signup Page',
-      imageUrl: '/assets/previews/challenges/signup-intro-preview',
-      description: 'Responsive signup form with real-time validation and error handling.',
-      technologies: ['HTML', 'CSS', 'JavaScript'],
-      level: 1,     
-      demoUrl: '/Challenges/intro-component-with-signup-form/',
+      title: "3-column preview card component",
+      imageUrl: "/assets/previews/challenges/3-column-preview",
+      description: "build out this 3-column preview card component.",
+      technologies: ["HTML", "CSS"],
+      level: 1,
+      demoUrl: "/Challenges/3-column-preview-card-component/",
       //  feSolutionLink: 'https://www.frontendmentor.io/solutions/',
-      gitRepoLink: 'https://github.com/kpmatlakala/Frontend-Mentor/tree/intro-component-with-sign-up-form'
+      gitRepoLink:
+        "https://github.com/kpmatlakala/Frontend-Mentor/tree/3-column-preview-card-component",
+      isGhost: true,
+      comingSoon: true,
     },
     // {
     //   title: 'Intro Component with Signup Page',
@@ -193,5 +227,5 @@ const projectsData = {
     //   isGhost: true,
     //   comingSoon: true
     // }
-  ]
-}; 
+  ],
+};
